e1582626e37a0ce8d8d386114dee28719d1c1158
Prakashchandra Rokade <prakashchandrar@hexaware.com>
2026-07-08 17:55:36 +0530

    new error added.

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -12,10 +12,10 @@ public class Calculator
     {
         if (b == "0")
             throw new DivideByZeroException("Cannot divide by zero.");
-        return a / b
+        return a / b;
     }
 
-    public double Modulo(string a, double b)
+    public double Modulo(double a, double b)
     {
         if (b == 0)
             throw new DivideByZeroException("Cannot modulo by zero.");
