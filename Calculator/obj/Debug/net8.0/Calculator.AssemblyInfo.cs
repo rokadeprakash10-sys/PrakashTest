0184254c352da142b904892d2e8ee87a16168b7d
Prakashchandra Rokade <prakashchandrar@hexaware.com>
2026-07-09 20:26:55 +0530

    ok

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7071e18c746c8e0b1148392df13f889f9ad6dd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b7e7735f09c1c96cdb09bc8e81cc135dfc3d35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
