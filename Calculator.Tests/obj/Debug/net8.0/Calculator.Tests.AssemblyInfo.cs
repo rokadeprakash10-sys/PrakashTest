809564ec0128c9cb4a9feedaf8a585cdf1e75fe9
Prakashchandra Rokade <prakashchandrar@hexaware.com>
2026-07-09 11:07:08 +0530

    semicolumn missing

--- Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
+++ Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53a804eb43cd57f36bbfd282caf7dbbc62fae13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d776e64e7e98c7c915c8fe5c29fba21f246c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
