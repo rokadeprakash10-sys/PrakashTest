1ea60e7b8cbdb52c839df8cdb4081cf7e07a94b9
Prakashchandra Rokade <prakashchandrar@hexaware.com>
2026-07-11 00:32:42 +0530

    OK IYS GOOD

--- Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
+++ Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2054bece054b0acb912b9df2623d8f4df782d6b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d544a4ee9c6c328cd2de70376761161fc641f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
