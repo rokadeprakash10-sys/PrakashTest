8fa17493b20407c7e362d05faabe460a74f045be
Prakashchandra Rokade <prakashchandrar@hexaware.com>
2026-07-10 10:45:02 +0530

    new test 7

--- Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
+++ Calculator.Tests/obj/Debug/net8.0/Calculator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4978b82ad8072af847a607ac5efe5574c53cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7766441d613c2aa70d07c7b6447bee223301b9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4978b82ad8072af847a607ac5efe5574c53cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7766441d613c2aa70d07c7b6447bee223301b9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
